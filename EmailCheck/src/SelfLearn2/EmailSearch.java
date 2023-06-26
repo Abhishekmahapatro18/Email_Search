3d57886be36c0e8f60d362b56c6206926bbb84ba
Abhishekmahapatro18 <137686541+Abhishekmahapatro18@users.noreply.github.com>
2023-06-26 19:48:54 +0530

    V 1.1
    Added multiple mail ids to check.

--- EmailCheck/src/SelfLearn2/EmailSearch.java
+++ EmailCheck/src/SelfLearn2/EmailSearch.java
@@ -7,11 +7,11 @@ import java.util.Scanner;
 public class EmailSearch {
     public static void main(String[] args) {
         List<String> emailList = new ArrayList<>();
-        emailList.add("john@example.com");
-        emailList.add("jane@example.com");
-        emailList.add("alex@example.com");
-        emailList.add("sam@example.com");
-        emailList.add("mary@example.com");
+        emailList.add("abhishek@gmail.com");
+        emailList.add("vickey@gmail.com");
+        emailList.add("abhishek@gmail.com");
+        emailList.add("mahapatro@gmail.com");
+        emailList.add("india@gmail.com");
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter an email ID to search: ");
